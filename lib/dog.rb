870095f84815d8f681ff9da455289c455a0649eb
Nate Miller <natekmiller96@gmail.com>
2018-07-03 21:10:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  def name
+    return @name
+  end
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
+require 'pry'
 class Owner
-  # code goes here
+  @@all = []
+  attr_accessor :pets, :name
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @name = name
+    @species = species
+    @@all << self
+  end
+  def species
+    return @species
+  end
+  def say_species
+    return "I am a #{@species}."
+  end
+  def self.all
+    return @@all
+  end
+  def self.reset_all
+    @@all = []
+  end
+  def self.count
+    return @@all.length
+  end
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  def walk_dogs
+    self.pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+  def feed_fish
+    self.pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+  def play_with_cats
+    self.pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+  def list_pets
+    dog_num = self.pets[:dogs].length
+    cat_num = self.pets[:cats].length
+    fish_num = self.pets[:fishes].length
+    return "I have #{fish_num} fish, #{dog_num} dog(s), and #{cat_num} cat(s)."
+  end
+  def sell_pets
+    self.pets[:cats].each do |cat|
+      cat.mood = "nervous"
+    end
+    self.pets[:fishes].each do |fish|
+      fish.mood = "nervous"
+    end
+    self.pets[:dogs].each do |dog|
+      dog.mood = "nervous"
+    end
+    @pets = []
+  end
 end
